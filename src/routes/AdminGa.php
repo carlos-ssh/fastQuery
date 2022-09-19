85e9d1c54df208a59ecfe276252cf306c84624e7
Carlos Flores <carlosflores@Carloss-MacBook-Pro.local>
2022-09-19 05:50:50 -0500

    create a route that validates if the user id that comes from client is an admin and validates if the user has permissons to do queries at the db

--- src/routes/AdminGa.php
+++ src/routes/AdminGa.php
@@ -11,31 +11,137 @@ use \Psr\Http\Message\ResponseInterface as Response;
 $app = new \Slim\App;
 
 //GET /ga-admins/all
+//$app->get(
+//    '/api/ga-admins/all',
+//    function (Request $request, Response $response) {
+//        $body = json_decode($request->getBody()->getContents(), true);
+//        $admin_client_id = isset($body['ga_admin_from_client']) ? $body['ga_admin_from_client'] : null;
+//        $haveThePower = isset($body['TID_IsGAADMIN']) ? $body['TID_IsGAADMIN'] : null;
+//
+//        if (is_null($admin_client_id)) {
+//            $response->getBody()->write(json_encode(array('error' => 'No se ha enviado el dato correctamente')));
+//            return $response->withStatus(400);
+//        } elseif ($admin_client_id && $haveThePower == 1) {
+//            $sql="
+//                SELECT TID_TalentID, CONCAT(TIN_Name, TIN_Last_Name) AS name, TIN_Email, TID_IsGAADMIN
+//                FROM TalentID AS tid
+//                LEFT JOIN TalentId_Info AS tii
+//                ON tii.TIN_ID = tid.TID_TalentID
+//                WHERE tid.TID_TalentID
+//            ";
+//            $db = new db();
+//            $db = $db->connectBD();
+//            $stm = $db->prepare($sql);
+//            $stm->execute([]);
+//            echo $stm->execute([]);
+//            $isAdmin = $stm->fetchAll(PDO::FETCH_OBJ);
+//            $response->getBody()->write(json_encode($isAdmin));
+//        } else {
+//            $response->getBody()->write(json_encode(array('error' => 'No tienes permiso para esta operacion.')));
+//            return $response;
+//        }
+//    }
+//);
+
+//$app->get(
+//    '/api/ga-admins/{id}',
+//    function (Request $request, Response $response) {
+//        $body = json_decode($request->getBody()->getContents(), true);
+//        $admin_client_id = isset($body['ga_admin_from_client']) ? $body['ga_admin_from_client'] : null;
+//        $id = $request->getAttribute($admin_client_id);
+//        $haveThePower = isset($body['TID_IsGAADMIN']) ? $body['TID_IsGAADMIN'] : null;
+//
+//        if (is_null($id)) {
+//            $response->getBody()->write(json_encode(array('error' => 'No se ha enviado el dato correctamente aqui')));
+//            return $response->withStatus(400, $id);
+//        } elseif ($id && $haveThePower == 1) {
+//            $sql="
+//                SELECT TID_TalentID, CONCAT(TIN_Name, TIN_Last_Name) AS name, TIN_Email, TID_IsGAADMIN
+//                FROM TalentID AS tid
+//                LEFT JOIN TalentId_Info AS tii
+//                ON tii.TIN_ID = tid.TID_TalentID
+//                WHERE tid.TID_TalentID = $id
+//            ";
+//            $db = new db();
+//            $db = $db->connectBD();
+//            $stm = $db->prepare($sql);
+//            $stm->execute(['id' => $id]);
+//            echo $stm->execute(['id' => $id]);
+//            $org_stats = $stm->fetchAll(PDO::FETCH_OBJ);
+//            $response->getBody()->write(json_encode($org_stats));
+//        } elseif($haveThePower == 0) {
+//            $response->getBody()->write(json_encode(array('error' => 'Lo siento. No tienes suficientes permisos.')));
+//            return $response;
+//        } else {
+//            $response->getBody()->write(json_encode(array('error' => 'No tienes permiso para esta operación.')));
+//            return $response;
+//        }
+//    }
+//);
+// GET /ga-admins/{id}
+
+// POST /ga-admins/create
+//$app->post(
+//    '/api/ga-admins/create',
+//    function (Request $request, Response $response) {
+//        $body = $request->getParsedBody();
+//        $new_admin_client = isset($body['ga_admin_from_client']) ? $body['ga_admin_from_client'] : null;
+//        $id = $request->getAttribute($new_admin_client);
+//        $sql="
+//            INSERT INTO
+//                TalentId_Info (TIN_Name, TIN_Last_Name, TIN_Email)
+//                TalentID (TID_IsGAADMIN)
+//            VALUES (:TIN_Name :TIN_Last_Name, :TIN_Email, :TID_IsGAADMIN)
+//        ";
+//        $db = new db();
+//        $db = $db->connectBD();
+//        $stm = $db->prepare($sql);
+//        $stm->execute(['id' => $id]);
+//        echo $stm->execute(['id' => $id]);
+//        $org_stats = $stm->fetchAll(PDO::FETCH_OBJ);
+//        $response->getBody()->write(json_encode($org_stats));
+//    }
+//);
+
 $app->get(
     '/api/ga-admins/all',
     function (Request $request, Response $response) {
         $body = json_decode($request->getBody()->getContents(), true);
-        $admin_client_id = isset($body['ga_admin_from_client']) ? $body['ga_admin_from_client'] : null;
-        $haveThePower = isset($body['TID_IsGAADMIN']) ? $body['TID_IsGAADMIN'] : null;
+        $admin_id = isset($body['ga_admin']) ? $body['ga_admin'] : null;
 
-        if (is_null($admin_client_id)) {
+        if (is_null($admin_id)) {
             $response->getBody()->write(json_encode(array('error' => 'No se ha enviado el dato correctamente')));
             return $response->withStatus(400);
-        } elseif ($admin_client_id && $haveThePower == 1) {
-            $sql="
-                SELECT TID_TalentID, CONCAT(TIN_Name, TIN_Last_Name) AS name, TIN_Email, TID_IsGAADMIN
-                FROM TalentID AS tid
-                LEFT JOIN TalentId_Info AS tii
-                ON tii.TIN_ID = tid.TID_TalentID
-                WHERE tid.TID_TalentID
+        } elseif ($admin_id) {
+            // validate if the id that comes from the client is an admin
+            $sql = "
+            SELECT EXISTS (SELECT TID_TalentID, TID_IsGAADMIN FROM TalentID WHERE TID_IsGAADMIN=1 AND TID_TalentID='$admin_id' ) AS isAdmin
             ";
-            $db = new db();
-            $db = $db->connectBD();
-            $stm = $db->prepare($sql);
-            $stm->execute([]);
-            echo $stm->execute([]);
-            $isAdmin = $stm->fetchAll(PDO::FETCH_OBJ);
-            $response->getBody()->write(json_encode($isAdmin));
+            $db_access = new db();
+            $db_access = $db_access->connectBD();
+
+            $stm_admin = $db_access->prepare($sql);
+            $stm_admin->execute();
+            $canAccess = $stm_admin->fetch();
+            if ($canAccess['isAdmin'] == 1) {
+                $sql="
+                    SELECT TID_TalentID, CONCAT(TIN_Name, TIN_Last_Name) AS name, TID_Email, TID_IsGAADMIN
+                    FROM TalentID AS tid
+                    LEFT JOIN TalentId_Info AS tii
+                    ON tii.TIN_TID_ID = tid.TID_TalentID
+                    WHERE tid.TID_IsGAADMIN=1
+                ";
+                $db = new db();
+                $db = $db->connectBD();
+                $stm = $db->prepare($sql);
+                $stm->execute([]);
+                echo $stm->execute([]);
+                $isAdmin = $stm->fetchAll(PDO::FETCH_OBJ);
+                $response->getBody()->write(json_encode($isAdmin));
+            } else {
+                $response->getBody()->write(json_encode(array('error' => 'No tienes permiso para esta operacion.')));
+                return $response;
+            }
         } else {
             $response->getBody()->write(json_encode(array('error' => 'No tienes permiso para esta operacion.')));
             return $response;
@@ -43,83 +149,56 @@ $app->get(
     }
 );
 
-// GET /ga-admins/{id}
-$app->get(
-    '/api/ga-admins/{id}',
+// create a post request to create a new organization and validate if the user has permission to do it
+// POST /organizations/create
+$app->post(
+    '/api/organizations/create',
     function (Request $request, Response $response) {
         $body = json_decode($request->getBody()->getContents(), true);
-        $admin_client_id = isset($body['ga_admin_from_client']) ? $body['ga_admin_from_client'] : null;
-        $id = $request->getAttribute($admin_client_id);
-        $haveThePower = isset($body['TID_IsGAADMIN']) ? $body['TID_IsGAADMIN'] : null;
+        $org_name = $body['org_name'] ?? null;
+        $org_email = $body['org_email'] ?? null;
+        $org_phone = $body['org_phone'] ?? null;
+        $org_address = $body['org_address'] ?? null;
+        $org_city = $body['org_city'] ?? null;
+        $org_state = $body['org_state'] ?? null;
+        $org_zip = $body['org_zip'] ?? null;
+        $org_country = $body['org_country'] ?? null;
+        $org_admin = $body['org_admin'] ?? null;
 
-        if (is_null($id)) {
-            $response->getBody()->write(json_encode(array('error' => 'No se ha enviado el dato correctamente aqui')));
-            return $response->withStatus(400, $id);
-        } elseif ($id && $haveThePower == 1) {
-            $sql="
-                SELECT TID_TalentID, CONCAT(TIN_Name, TIN_Last_Name) AS name, TIN_Email, TID_IsGAADMIN
-                FROM TalentID AS tid
-                LEFT JOIN TalentId_Info AS tii
-                ON tii.TIN_ID = tid.TID_TalentID
-                WHERE tid.TID_TalentID = $id
+        if (is_null($org_name) || is_null($org_email) || is_null($org_phone) || is_null($org_address) || is_null($org_city) || is_null($org_state) || is_null($org_zip) || is_null($org_country) || is_null($org_admin)) {
+            $response->getBody()->write(json_encode(array('error' => 'No se ha enviado el dato correctamente')));
+            return $response->withStatus(400);
+        } elseif ($org_name && $org_email && $org_phone && $org_address && $org_city && $org_state && $org_zip && $org_country && $org_admin) {
+            // validate if the id that comes from the client is an admin
+            $sql = "
+            SELECT EXISTS (SELECT TID_TalentID, TID_IsGAADMIN FROM TalentID WHERE TID_IsGAADMIN=1 AND TID_TalentID='$org_admin' ) AS isAdmin
             ";
-            $db = new db();
-            $db = $db->connectBD();
-            $stm = $db->prepare($sql);
-            $stm->execute(['id' => $id]);
-            echo $stm->execute(['id' => $id]);
-            $org_stats = $stm->fetchAll(PDO::FETCH_OBJ);
-            $response->getBody()->write(json_encode($org_stats));
-        } elseif($haveThePower == 0) {
-            $response->getBody()->write(json_encode(array('error' => 'Lo siento. No tienes suficientes permisos.')));
-            return $response;
+            $db_access = new db();
+            $db_access = $db_access->connectBD();
+
+            $stm_admin = $db_access->prepare($sql);
+            $stm_admin->execute();
+            $canAccess = $stm_admin->fetch();
+            if ($canAccess['isAdmin'] == 1) {
+                $sql="
+                    INSERT INTO
+                        Organization (ORG_Name, ORG_Email, ORG_Phone, ORG_Address, ORG_City, ORG_State, ORG_Zip, ORG_Country)
+                    VALUES (:ORG_Name, :ORG_Email, :ORG_Phone, :ORG_Address, :ORG_City, :ORG_State, :ORG_Zip, :ORG_Country)
+                ";
+                $db = new db();
+                $db = $db->connectBD();
+                $stm = $db->prepare($sql);
+                $stm->execute(['org_name' => $org_name, 'org_email' => $org_email, 'org_phone' => $org_phone, 'org_address' => $org_address, 'org_city' => $org_city, 'org_state' => $org_state, 'org_zip' => $org_zip, 'org_country' => $org_country]);
+                echo $stm->execute(['org_name' => $org_name, 'org_email' => $org_email, 'org_phone' => $org_phone, 'org_address' => $org_address, 'org_city' => $org_city, 'org_state' => $org_state, 'org_zip' => $org_zip, 'org_country' => $org_country]);
+                $org_stats = $stm->fetchAll(PDO::FETCH_OBJ);
+                $response->getBody()->write(json_encode($org_stats));
+            } else {
+                $response->getBody()->write(json_encode(array('error' => 'No tienes permiso para esta operacion.')));
+                return $response;
+            }
         } else {
-            $response->getBody()->write(json_encode(array('error' => 'No tienes permiso para esta operación.')));
+            $response->getBody()->write(json_encode(array('error' => 'No tienes permiso para esta operacion.')));
             return $response;
         }
     }
 );
-
-// POST /ga-admins/create
-$app->post(
-    '/api/ga-admins/create',
-    function (Request $request, Response $response) {
-        $body = $request->getParsedBody();
-        $new_admin_client = isset($body['ga_admin_from_client']) ? $body['ga_admin_from_client'] : null;
-        $id = $request->getAttribute($new_admin_client);
-        $sql="
-            INSERT INTO 
-                TalentId_Info (TIN_Name, TIN_Last_Name, TIN_Email)
-                TalentID (TID_IsGAADMIN)
-            VALUES (:TIN_Name :TIN_Last_Name, :TIN_Email, :TID_IsGAADMIN)
-        ";
-        $db = new db();
-        $db = $db->connectBD();
-        $stm = $db->prepare($sql);
-        $stm->execute(['id' => $id]);
-        echo $stm->execute(['id' => $id]);
-        $org_stats = $stm->fetchAll(PDO::FETCH_OBJ);
-        $response->getBody()->write(json_encode($org_stats));
-    }
-);
-
-// create a new admin account and send data into the database
-// POST /ga-admins/create
-
-// PUT /ga-admins/edit/{id}
-
-// DELETE /ga-admins/delete/{id}
-
-// admin: 003d2335-9422-11ec-9e65-00163efa374e
-// non-admin: 9f318f44-9522-11ec-9e65-00163efa374e
-
-
-// {
-//     "ga_admin_from_client": "9f318f44-9522-11ec-9e65-00163efa374e",
-//     "TID_IsGAADMIN": 0
-// }
-// 
-// {
-//     "ga_admin_from_client": "003d2335-9422-11ec-9e65-00163efa374e",
-//     "TID_IsGAADMIN": 1
-// }
--- src/routes/queriesGA.sql
+++ src/routes/queriesGA.sql
@@ -1,7 +1,31 @@
-select 
-    Cha_ID, Cha_Name,
-    345*Challenges.Cha_ID as 'Mumero de proyectos',
-    34+Cha_ID as 'numero de equipos',
-    11+Challenges.Cha_ID as 'numero de mentores'
-from Challenges
-where Cha_Organizer=1;
+--select
+--    Cha_ID, Cha_Name,
+--    345*Challenges.Cha_ID as 'Mumero de proyectos',
+--    34+Cha_ID as 'numero de equipos',
+--    11+Challenges.Cha_ID as 'numero de mentores'
+--from Challenges
+--where Cha_Organizer=1;
+
+--SELECT IF(EXISTS(
+--    SELECT TID_TalentID, TID_IsGAADMIN
+--    FROM TalentID AS tid
+--    LEFT JOIN TalentId_Info AS tii
+--    ON tii.TIN_ID = $admin_client_id
+--    WHERE tid.TID_IsGAADMIN = 1
+--), 1, 0) AS isGAAdmin
+
+IF (SELECT TID_TalentID, TID_IsGAADMIN FROM TalentID AS tid WHERE tid.TID_TalentID="0055715c-9423-11ec-9e65-00163efa374e" AND tid.TID_IsGAADMIN=1) THEN
+--BEGIN
+--    SELECT TID_TalentID, CONCAT(TIN_Name, TIN_Last_Name) AS Name, TID_Email, TID_IsGAADMIN
+--    FROM TalentID AS tid
+--    LEFT JOIN TalentId_Info AS tii
+--    ON tii.TIN_TID_ID = tid.TID_TalentID
+--    WHERE TID_IsGAADMIN=1
+--END;
+
+--ELSE
+--BEGIN
+--    INSERT INTO Table (FieldValue) VALUES('');
+--    SELECT LAST_INSERT_ID() AS TableID;
+--END;
+--END IF;
